2724f71db3cda821ca206839a18e65561fdb7e26
Nhatnd <nhatnd@namvietmedia.com.vn>
2023-02-22 17:29:42 +0700

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import Layout from "../components/layout";
-import { Col, Row, Pagination } from "antd";
+import { Pagination, Tag, Space } from "antd";
 import PostList from "../components/postList";
 import axios from "axios";
 
@@ -33,125 +33,95 @@ export default function Home({ data }) {
           ))}
         </div>
 
-        <Row style={{ marginTop: 50 }}>
-          <Col span={16}>
+        <div className="grid grid-cols-3 gap-4">
+          <div class="col-span-2">
             <div className="grid gap-10 mt-10 lg:gap-10 md:grid-cols-2 xl:grid-cols-2 ">
               {posts?.map((post) => (
                 <PostList key={post.id} post={post} aspect="square" />
               ))}
             </div>
-            <Pagination
-              current={currentPage}
-              onChange={onChange}
-              total={data?.latestPosts.total}
-            />
-          </Col>
-          <Col span={7} style={{ marginLeft: 40 }}>
-            <div class="hidden py-2 xl:col-span-3 lg:col-span-4 md:hidden lg:block">
-              <div class=" space-x-5 border-b-2 border-opacity-10 dark:border-violet-400">
-                <p class=" text-xl font-bold uppercase border-b-1dark:border-violet-400">
-                  Xem nhiều
-                </p>
-              </div>
-              <div class="flex flex-col divide-y divide-gray-700">
-                <div class="flex px-1 py-4">
-                  <img
-                    alt=""
-                    class="flex-shrink-0 object-cover w-20 h-20 mr-4 dark:bg-gray-500"
-                    src="https://source.unsplash.com/random/244x324"
-                  />
-                  <div class="flex flex-col flex-grow">
-                    <a
-                      rel="noopener noreferrer"
-                      href="#"
-                      class="font-serif hover:underline"
-                    >
-                      Aenean ac tristique lorem, ut mollis dui.
-                    </a>
-                    <p class="mt-auto text-xs dark:text-gray-400">
-                      5 minutes ago
-                      <a
-                        rel="noopener noreferrer"
-                        href="#"
-                        class="block dark:text-blue-400 lg:ml-2 lg:inline hover:underline"
-                      >
-                        Politics
-                      </a>
-                    </p>
-                  </div>
-                </div>
-                <div class="flex px-1 py-4">
-                  <img
-                    alt=""
-                    class="flex-shrink-0 object-cover w-20 h-20 mr-4 dark:bg-gray-500"
-                    src="https://source.unsplash.com/random/245x325"
-                  />
-                  <div class="flex flex-col flex-grow">
-                    <a href="#" class="font-serif hover:underline">
-                      Nulla consectetur efficitur.
-                    </a>
-                    <p class="mt-auto text-xs dark:text-gray-400">
-                      14 minutes ago
-                      <a
-                        href="#"
-                        class="block dark:text-blue-400 lg:ml-2 lg:inline hover:underline"
-                      >
-                        Sports
-                      </a>
-                    </p>
-                  </div>
-                </div>
-                <div class="flex px-1 py-4">
-                  <img
-                    alt=""
-                    class="flex-shrink-0 object-cover w-20 h-20 mr-4 dark:bg-gray-500"
-                    src="https://source.unsplash.com/random/246x326"
-                  />
-                  <div class="flex flex-col flex-grow">
-                    <a href="#" class="font-serif hover:underline">
-                      Vitae semper augue purus tincidunt libero.
-                    </a>
-                    <p class="mt-auto text-xs dark:text-gray-400">
-                      22 minutes ago
-                      <a
-                        href="#"
-                        class="block dark:text-blue-400 lg:ml-2 lg:inline hover:underline"
-                      >
-                        World
-                      </a>
-                    </p>
-                  </div>
-                </div>
-                <div class="flex px-1 py-4">
-                  <img
-                    alt=""
-                    class="flex-shrink-0 object-cover w-20 h-20 mr-4 dark:bg-gray-500"
-                    src="https://source.unsplash.com/random/247x327"
-                  />
-                  <div class="flex flex-col flex-grow">
-                    <a
-                      rel="noopener noreferrer"
-                      href="#"
-                      class="font-serif hover:underline"
-                    >
-                      Suspendisse potenti.
-                    </a>
-                    <p class="mt-auto text-xs dark:text-gray-400">
-                      37 minutes ago
-                      <a
-                        rel="noopener noreferrer"
-                        href="#"
-                        class="block dark:text-blue-400 lg:ml-2 lg:inline hover:underline"
-                      >
-                        Business
-                      </a>
-                    </p>
-                  </div>
-                </div>
-              </div>
+            <div className="mt-6">
+              <Pagination
+                current={currentPage}
+                onChange={onChange}
+                total={data?.latestPosts.total}
+              />
             </div>
-          </Col>
-        </Row>
+          </div>
+          <div>
+            <div class="hidden py-2 xl:col-span-3 lg:col-span-4 md:hidden lg:block">
+              <section>
+                <div class=" space-x-5 border-b-2 border-opacity-10 dark:border-violet-400">
+                  <p class=" text-xl font-bold uppercase border-b-1dark:border-violet-400">
+                    Xem nhiều
+                  </p>
+                </div>
+                <div class="flex flex-col divide-y divide-gray-700">
+                  {Array.apply(0, Array(5)).map(function (x, i) {
+                    return (
+                      <div class="flex px-1 py-4 ">
+                        <img
+                          alt=""
+                          class="flex-shrink-0 object-cover w-20 rounded-lg h-20 mr-4 dark:bg-gray-500"
+                          src="https://source.unsplash.com/random/244x324"
+                        />
+                        <div class="flex flex-col flex-grow">
+                          <a
+                            rel="noopener noreferrer"
+                            href="#"
+                            class="font-serif hover:underline"
+                          >
+                            Aenean ac tristique lorem, ut mollis dui.
+                          </a>
+                          <p class="mt-auto text-xs dark:text-gray-400">
+                            5 minutes ago
+                            <a
+                              rel="noopener noreferrer"
+                              href="#"
+                              class="block dark:text-blue-400 lg:ml-2 lg:inline hover:underline"
+                            >
+                              Politics
+                            </a>
+                          </p>
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              </section>
+
+              <section className="mt-6">
+                <div class=" space-x-5 border-b-2 border-opacity-10 dark:border-violet-400">
+                  <p class=" text-xl font-bold uppercase border-b-1dark:border-violet-400">
+                    Thẻ phổ biến
+                  </p>
+                </div>
+                <div class="flex flex-col divide-y divide-gray-700">
+                  <div class="mt-6">
+                    <a
+                      href="#"
+                      class="ml-4 mb-3 text-xs inline-flex items-center font-bold leading-sm uppercase px-3 py-1 rounded-full bg-white text-gray-700 border"
+                    >
+                      Thuat toan
+                    </a>
+                    <a
+                      href="#"
+                      class="ml-4 text-xs inline-flex items-center font-bold leading-sm uppercase px-3 py-1 rounded-full bg-white text-gray-700 border"
+                    >
+                      C#
+                    </a>
+                    <a
+                      href="#"
+                      class="ml-4 text-xs inline-flex items-center font-bold leading-sm uppercase px-3 py-1 rounded-full bg-white text-gray-700 border"
+                    >
+                      .NET
+                    </a>
+                  </div>
+                </div>
+              </section>
+            </div>
+          </div>
+        </div>
       </div>
     </Layout>
   );
